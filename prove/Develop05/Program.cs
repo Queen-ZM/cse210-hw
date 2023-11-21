fef0fd834b22db3b716c83749810dcbf9ce81084
zil22002@byui.edu <zilimbolanoluthando1991@gmail.com>
2023-11-21 19:02:58 +0200

    modification

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -10,9 +10,9 @@ class Program
             Console.WriteLine("Menu:");
             Console.WriteLine("1. Create New Goal");
             Console.WriteLine("2. List Goals");
-            Console.WriteLine("3. Save Goals to txt file");
+            Console.WriteLine("3. Save Goals");
             Console.WriteLine("4. Load Goals");
-            Console.WriteLine("5. Record Event (Mark Completed)");
+            Console.WriteLine("5. Record Event");
             Console.WriteLine("6. Exit");
 
             int choice = GetIntInput("Enter your choice: ");
